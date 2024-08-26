7793611c178c025e59e6577ac6cfb8153f7448b5
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-08-26 16:29:58 +0200

    vitest setup.

---
+++ vitest.config.js
@@ -0,0 +1,13 @@
+import config from "./vite.config";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    plugins: [...config.plugins],
+
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: './test/vitest.setup.js',
+        css: true,
+      },
+})
