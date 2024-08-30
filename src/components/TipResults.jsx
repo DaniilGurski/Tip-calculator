b25f30e8a72272fb46d417f6ebd8d5b61826c0c8
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-08-30 17:54:38 +0200

    refactor.

--- src/components/TipResults.jsx
+++ src/components/TipResults.jsx
@@ -1,40 +1,33 @@
 import { useContext, useEffect, useState } from "react"
 import { CALC_ACTIONS } from "../utils/calculatorActions";
 import { calculatorContext } from "../utils/calculatorContext";
-
+import { calculateTip } from "../utils/helpers";
 
 export default function TipResults() {
   const { form, dispatch } = useContext(calculatorContext); 
   let { bill, people, currentPercentage, formValid} = form;
 
-  let tipAmount = "0.00$";
-  let totalPerPerson = "0.00$";
-
-  if (formValid) {
-    bill = parseInt(bill);
-    people = parseInt(people);
-    const tip = bill * (currentPercentage / 100);
-    
-    tipAmount = `${(tip / people).toFixed(2)}$`;
-    totalPerPerson = `${((bill + tip) / people).toFixed(2)}$`;
-  }
+  const {amountPerPerson, totalAmountPerPerson} = calculateTip(formValid, bill, people, currentPercentage);
 
   return (
     <div className="tip-results bg-dark-cyan clr-white">
       <ul className="tip-results__sums grid" role="list">
         <li>
           <h2> Tip Amount </h2>
-          <span className="clr-accent fs-xl"> {tipAmount} </span>
+          <span className="clr-accent fs-xl" id="tip-amount">{`${amountPerPerson}$`}</span>
         </li>
         <li>
           <h2> Total </h2>
-          <span className="clr-accent fs-xl"> {totalPerPerson} </span>
+          <span className="clr-accent fs-xl" id="total-tip-amount">{`${totalAmountPerPerson}$`}</span>
         </li>
 
       </ul>
-      <button className="tip-results__reset bg-accent clr-dark-cyan fs-large" onClick={() => {
+      <button 
+      className="tip-results__reset bg-accent clr-dark-cyan fs-large" 
+      onClick={() => {
         dispatch({type: CALC_ACTIONS.RESET_STATES})
-      }}> RESET </button>
+      }}
+      aria-label="reset calculator"> RESET </button>
     </div>
   )
 }
