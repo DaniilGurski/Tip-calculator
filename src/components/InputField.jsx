5b3361332b3c150099a9128de805fbb8259a4964
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-08-30 17:52:39 +0200

    error message refactor.

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -4,12 +4,13 @@ import { calculatorContext } from "../utils/calculatorContext"
 
 export default function InputField({ labelText, inputIcon, value, action}) {
   const { dispatch } = useContext(calculatorContext);
+  const isInputValid = (value !== "0")
 
   return (
     <div>
       <label className="clr-label" htmlFor={labelText.toLowerCase()}> {labelText} </label>
 
-      <div className={`input-container ${value === "0" ? "input-container--error" : ""} fs-large`} data-input-icon={inputIcon}>
+      <div className={`input-container ${!isInputValid ? "input-container--error" : ""} fs-large`} data-input-icon={inputIcon}>
         <input
           id={labelText.toLowerCase()}
           type="number"
@@ -17,6 +18,10 @@ export default function InputField({ labelText, inputIcon, value, action}) {
           value={value}
           onChange={(e) => dispatch({type: action, payload: e.target.value})}
         />
+
+        <div className="input-container__error clr-error" aria-live="assertive" id="error-message">
+          {!isInputValid && <span className="fs-base"> Can't be zero </span>}
+        </div>
       </div>
     </div>
   )
